cf6a4f6b295962dbbe78bdc5ccc9198a58628b57
golmaker <iv7.software@gmail.com>
2021-10-28 21:31:30 -0300

    Styles, texts & more

--- packages/gol-uikit/src/components/Svg/Icons/PancakeRound.tsx
+++ packages/gol-uikit/src/components/Svg/Icons/PancakeRound.tsx
@@ -6,7 +6,7 @@ const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 90 90" {...props}>
       <circle cx="45" cy="45" r="45" fill="url(#paint0_linear_pancakeround)" />
-      <image href="/logo.png"/>
+      <image href="/logo.png" width="92"/>
       <defs>
         <linearGradient id="paint0_linear_pancakeround" x1="45" y1="0" x2="45" y2="90" gradientUnits="userSpaceOnUse">
           <stop stopColor="#54DADE" />
