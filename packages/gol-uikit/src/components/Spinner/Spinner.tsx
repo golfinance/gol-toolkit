2dd2e2592fc754022928bf46da07b4ae66209f0d
golmaker <iv7.software@gmail.com>
2021-11-05 18:15:57 -0300

    fix Spinner icon

--- packages/gol-uikit/src/components/Spinner/Spinner.tsx
+++ packages/gol-uikit/src/components/Spinner/Spinner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import PanIcon from "./PanIcon";
+// import PanIcon from "./PanIcon";
 import PancakeIcon from "./PancakeIcon";
 import { SpinnerProps } from "./types";
 
@@ -13,17 +13,17 @@ const rotate = keyframes`
   }
 `;
 
-const float = keyframes`
-	0% {
-		transform: translatey(0px);
-	}
-	50% {
-		transform: translatey(10px);
-	}
-	100% {
-		transform: translatey(0px);
-	}
-`;
+// const float = keyframes`
+// 	0% {
+// 		transform: translatey(0px);
+// 	}
+// 	50% {
+// 		transform: translatey(10px);
+// 	}
+// 	100% {
+// 		transform: translatey(0px);
+// 	}
+// `;
 
 const Container = styled.div`
   position: relative;
@@ -37,16 +37,16 @@ const RotatingPancakeIcon = styled(PancakeIcon)`
   transform: translate3d(0, 0, 0);
 `;
 
-const FloatingPanIcon = styled(PanIcon)`
-  animation: ${float} 6s ease-in-out infinite;
-  transform: translate3d(0, 0, 0);
-`;
+// const FloatingPanIcon = styled(PanIcon)`
+//   animation: ${float} 6s ease-in-out infinite;
+//   transform: translate3d(0, 0, 0);
+// `;
 
 const Spinner: React.FC<SpinnerProps> = ({ size = 128 }) => {
   return (
     <Container>
       <RotatingPancakeIcon width={`${size * 0.5}px`} />
-      <FloatingPanIcon width={`${size}px`} />
+      {/* <FloatingPanIcon width={`${size}px`} /> */}
     </Container>
   );
 };
